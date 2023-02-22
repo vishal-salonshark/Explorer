a3dd5b84f80ee77aea813f20f87dc3416e4903d6
vishal0298 <vishalpawar0298@gmail.com>
2023-02-22 11:16:27 +0530

    Feb21 commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,22 +1,23 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, {useState} from "react";
 // import { BrowserRouter as Router } from 'react-router-dom';
 import  { GiCubes, GiToken } from "react-icons/gi";
 import { IoGitNetworkSharp } from "react-icons/io5";
 import  { BiTransferAlt } from "react-icons/bi";
-import  { BsSearch } from "react-icons/bs";
+// import  { BsSearch } from "react-icons/bs";
 import { AiFillDatabase } from "react-icons/ai";
 import Logo from "./../img/Erium_logo.png"
-import Search from './../components/Search'
+// import Search from './../components/Search'
 
 const Navbar = () => {
-    const [modalOn, setModalOn] = useState(false)
-    // eslint-disable-next-line
-    const [choice, setChoice] = useState(false)
+    // const [modalOn, setModalOn] = useState(false)
+    // // eslint-disable-next-line
+    // const [choice, setChoice] = useState(false)
   
-    const clicked = () => {
-      setModalOn(true)
-    }
+    // const clicked = () => {
+    //   setModalOn(true)
+    // }
     return (
         // <Router>
         <nav className="flex items-center justify-between px-14 py-2 bg-transparent border-b-2 border-[#e2e3e4] text-slate-50 h-16">
@@ -30,10 +31,10 @@ const Navbar = () => {
                     {/* <a href="/user" className="flex justify-between items-center space-x-1"><IoWallet/><label> Users </label></a> */}
                     <a href="" className="flex justify-between items-center space-x-1"><IoGitNetworkSharp/><label> EriumChain </label></a>
                 </ul>
-                <div className="flex items-center px-6 py-1 rounded-full justify-center bg-[#89CDB3] text-slate-50 space-x-2">
+                {/* <div className="flex items-center px-6 py-1 rounded-full justify-center bg-[#89CDB3] text-slate-50 space-x-2">
                     <BsSearch/><label className=" text-center font-bold" onClick={() => clicked()}>Search</label>
                     {modalOn && <Search setModalOn={setModalOn} setChoice={setChoice} />}
-                </div>
+                </div> */}
                     
             </div>
         </nav>
